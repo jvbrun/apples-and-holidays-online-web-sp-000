d4125cb9d77f77da94b2f83ee9596417ba54fd31
Jean-Victor Brun <jeanvbrun@icloud.com>
2021-01-03 04:03:42 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -28,7 +28,7 @@ def add_supply_to_winter_holidays(holiday_hash, supply)
    {
     holiday_hash.each do |season, holiday|
   if season == "winter"
-    holiday.each do | {holiday.index + 1}
+    holiday.each do | 
     end
   end
 end
